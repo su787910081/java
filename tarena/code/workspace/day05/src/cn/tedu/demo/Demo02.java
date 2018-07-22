a033bf62d942f92f1a7206667ea44e8ee0577c1d
suyh <su_787910081@sina.com>
2018-07-22 15:22:12 +0800

    com

---
+++ tarena/code/workspace/day05/src/cn/tedu/demo/Demo02.java
@@ -0,0 +1,13 @@
+package cn.tedu.demo;
+
+public class Demo02 {
+	public static void main(String[] args) {
+		Demo02 d = new Demo02();
+		d.demo();
+		d.demo(1, 3, 3);
+	}
+	
+	public void demo(int...is) {
+		System.out.println();
+	}
+}
