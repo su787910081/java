ff3b1d46fda43eb415e60731a7c26e8650a75a9f
suyh <su_787910081@sina.com>
2018-07-29 16:19:52 +0800

    commit day03

---
+++ tarena/code/javaoop/day03/src/en/tedu/demo/ComputerTest.java
@@ -0,0 +1,13 @@
+package en.tedu.demo;
+
+public class ComputerTest {
+	public static void main(String[] args) {
+		Computer cpt = new Computer();
+		cpt.setName("ÁªÏë");
+		cpt.setNumber(10);
+		cpt.setPrice(5000);
+		cpt.setCpu("ËÄºË");
+		
+		System.out.println(cpt);
+	}
+}
---
+++ tarena/code/javaoop/day03/src/en/tedu/demo/StaticTest.java
@@ -0,0 +1,15 @@
+package en.tedu.demo;
+
+public class StaticTest {
+	
+	public static void main(String[] args) {
+		System.out.println("main begin.");
+//		StaticDemo demo = new StaticDemo();
+//		demo.demo02();
+		
+//		StaticDemo.demo01();
+	}
+	
+	public void test() {
+	}
+}
