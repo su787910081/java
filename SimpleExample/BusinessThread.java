6b202457762c084badf6e221b333a67646b865c4
su787910081 <su_787910081@sina.com>
2018-11-08 17:03:59 +0800

    Update BusinessThread.java
    添加唤醒与进入休眠功能
    这里要注意的是：所使用的notify 和wait 都一定是获取到锁的对象的方法。

--- SimpleExample/BusinessThread.java
+++ SimpleExample/BusinessThread.java
@@ -9,7 +9,8 @@ public class Main {
         Main m = new Main();
         
         try {
-            Thread.sleep(1);
+            System.out.println(Thread.currentThread().getname() + " sleep 10 seconds");
+            Thread.sleep(10000);
         } catch (Exception e) {
             // do nothing
         }
@@ -25,7 +26,17 @@ public class Main {
         // 以lambda 表达式的形式添加
         IFunc func2 = (String str) -> { m.func(str, 11); };
         
+        System.out.println(Thread.currentThread().getname() + " add first task");
         thread.add(func);
+        
+        try {
+            System.out.println(Thread.currentThread().getname() + " sleep 5 seconds");
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // do nothing
+        }
+        
+        System.out.println(Thread.currentThread().getname() + " add seconds task");
         thread.add(func2);
     }
     
@@ -40,23 +51,29 @@ import java.util.Queue;
 class BusinessThread extends Thread {
     @Override
     public void run() {
+        System.out.println("Thread begin...");
         queue = new LinkedList<IFunc>();
         str = Thread.getCurrentThread().getName();
         
         while (true) {
             IFunc f = null;
             synchronoized (queue) {
+                while (queue.size() == 0) {
+                    try {
+                        // 这里要进入到有锁的对象休眠，否则会报错
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        continue;
+                    }
+                    
+                    System.out.println("notify come on.");
+                }
+                
                 f = queue.poll();
             }
             
             if (f != null) {
                 f.func(str);
-            }
-            
-            try {
-                Thread.sleep(5000);
-            } catch (Exception e) {
-                // do nothing
             }
         }
     }
@@ -64,6 +81,7 @@ class BusinessThread extends Thread {
     public void add(IFunc func) {
         synchronoized (queue) {
             queue.add(func);
+            queue.notigyAll(); // 这里一样要使用获取锁的对象，来进行通知，否则也是报错，与wait() 相对应
         }
     }
 
