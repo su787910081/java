335a8a978d2088d2b66c0a43ff940d13a41fd131
suyh <su_787910081@sina.com>
2018-12-14 16:20:09 +0800

    添加规则，只有拥有class = "linkStair" 的span 标签 才有资格添加超链接 用来展开与关闭

--- tarena/note/NoteMain.js
+++ tarena/note/NoteMain.js
@@ -1,6 +1,8 @@
 /* 
  * 所有需要处理为超链接的元素标签(Element)必须紧跟着一个DIV 标签元素
  * 同时此Element 或者它的子元素至少有一个添加了class="linkStair"，且这个元素的内容将被直接处理成链接链接
+ * 添加一个规则，所有的超链接标签元素都必须放在一个span 标签里面，这样做的目的是防止其它标签里面有多个class 值时，元素内容被添加到a 标签里面而无法加载到css 的样式
+ * 即： 超链接必须 是span 标签 ，同时拥有class="linkstart"
  */
 
 var strDivPrevIdName = "divIdStair";    // 每一个需要展开或者隐藏的DIV ID 名前缀
@@ -114,14 +116,14 @@ function initLink(arrChilds, strCurDivPrevIdName) {
 // 指定节点元素是否为满足条件的超链接节点为，或者其子节点是否有满足条件的超链接节点
 function findLinkNode(curNode) {
     var linkNodes = [];
-    if (hasLinkStairClass(curNode)) {
+    if (enableLink(curNode)) {
         linkNodes.push(curNode);
     } else {
         var arrChilds = $(curNode).children();
         if (arrChilds) {
             for (var i = 0; i < arrChilds.length; i++) {
                 var ch = arrChilds[i];
-                if (hasLinkStairClass(ch)) {
+                if (enableLink(ch)) {
                     linkNodes.push(ch);
                 }
             }
@@ -131,15 +133,25 @@ function findLinkNode(curNode) {
     return linkNodes;
 }
 
-function hasLinkStairClass(curNode) {
-    if (curNode.className) {
+function enableLink(curNode) {
+    do {
+        if (!curNode) {
+            break;
+        }
+        if (!curNode.className) {
+            break;
+        }
+        if (curNode.tagName != "SPAN") {
+            break;
+        }
+
         var arrClasses = curNode.className.split(/[,\s]/); // 正则拆分子串
         for (var i = 0; i < arrClasses.length; i++) {
             if (arrClasses[i] == strLinkStair) {
                 return true;
             }
         }
-    }
+    } while (false);
 
     return false;
 }
