937178d3c1166991eb7efef15f09da7e2ea38f42
suyh <su_787910081@sina.com>
2019-07-27 09:24:05 +0800

    SpringCloud

---
+++ 后端/26_SpringCloud/_1803feginclient/src/main/java/com/jt/controller/ZullController.java
@@ -0,0 +1,15 @@
+package com.jt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ZullController {
+    @RequestMapping("/zuul")
+    @ResponseBody
+    public String getName(String name) {
+        return "hi, " + name + ", I am from feign";
+    }
+
+}
